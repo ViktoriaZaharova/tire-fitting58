390b868d49a4abb47ba2f3e6a72e42ea1d14c07d
viktoriazaharova <zaharova181114@gmail.com>
2020-11-13 18:05:20 +0200

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,110 @@
+$('.btn-burger').click(function () {
+   $('.header-bottom').fadeToggle();
+});
+
+$('.btn-close').click(function () {
+    $('.header-bottom').fadeOut();
+});
+
+// Инициализация карты
+ymaps.ready(init);
+
+function init () {
+
+    //Центрирование и выбор масштаба карты
+    var myMap = new ymaps.Map('map1', {
+        center: [55.753215, 37.622504],
+        zoom: 11
+    });
+
+    // Создание своей метки
+    var myPlacemark1 = new ymaps.Placemark(
+        // Координаты метки
+        [56.301833, 38.241398] , {
+            // Свойства метки
+            hintContent: '', //Подсказка при наведении на маркер
+            iconContent: '',
+
+        }, {
+            iconImageHref: 'img/loc.svg',  // картинка иконки
+            iconImageSize: [27, 46],                                      // размеры картинки
+            // iconImageOffset: [-70, -40],// смещение картинки
+
+        });
+
+    var myPlacemark2 = new ymaps.Placemark(
+        // Координаты метки
+        [55.733984, 37.586670] , {
+            // Свойства метки
+            hintContent: '', //Подсказка при наведении на маркер
+            iconContent: '',
+
+        }, {
+            iconImageHref: 'img/loc.svg',  // картинка иконки
+            iconImageSize: [27, 46],                                      // размеры картинки
+            // iconImageOffset: [-70, -40],// смещение картинки
+
+        });
+
+    var myPlacemark3 = new ymaps.Placemark(
+        // Координаты метки
+        [55.755814, 37.617635] , {
+            // Свойства метки
+            hintContent: '', //Подсказка при наведении на маркер
+            iconContent: '',
+
+        }, {
+            iconImageHref: 'img/loc.svg',  // картинка иконки
+            iconImageSize: [27, 46],                                      // размеры картинки
+            // iconImageOffset: [-70, -40],// смещение картинки
+
+        });
+
+    var myPlacemark4 = new ymaps.Placemark(
+        // Координаты метки
+        [55.771525, 37.683545] , {
+            // Свойства метки
+            hintContent: '', //Подсказка при наведении на маркер
+            iconContent: '',
+
+        }, {
+            iconImageHref: 'img/loc.svg',  // картинка иконки
+            iconImageSize: [27, 46],                                      // размеры картинки
+            // iconImageOffset: [-70, -40],// смещение картинки
+
+        });
+
+    var myPlacemark5 = new ymaps.Placemark(
+        // Координаты метки
+        [55.824172, 37.516521] , {
+            // Свойства метки
+            hintContent: '', //Подсказка при наведении на маркер
+            iconContent: '',
+
+        }, {
+            iconImageHref: 'img/loc.svg',  // картинка иконки
+            iconImageSize: [27, 46],                                      // размеры картинки
+            // iconImageOffset: [-70, -40],// смещение картинки
+
+        });
+
+    // Добавление метки на карту
+    myMap.geoObjects.add(myPlacemark1);
+    myMap.geoObjects.add(myPlacemark2);
+    myMap.geoObjects.add(myPlacemark3);
+    myMap.geoObjects.add(myPlacemark4);
+    myMap.geoObjects.add(myPlacemark5);
+
+    //Элементы управления
+    myMap.controls
+    // Кнопка изменения масштаба
+        .add('zoomControl')
+        // Список типов карты
+        .add('typeSelector')
+        // Кнопка изменения масштаба - справа
+        .add('smallZoomControl', { right: 5, top: 75 })
+        // Стандартный набор кнопок
+        .add('mapTools')
+        //Линейка масштаба
+        .add(new ymaps.control.ScaleLine());
+}
