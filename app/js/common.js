7ab217c1051c2f574a71fa18cd148f559b090245
viktoriazaharova <zaharova181114@gmail.com>
2020-11-16 15:10:41 +0200

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -46,6 +46,42 @@ $('.gallery-slider').slick({
     ]
 });
 
+// модальные окна (несколько)
+$(document).ready(function () {
+    var overlay = $('.overlay');
+    var open_modal = $('.open_modal');
+    var close = $('.modal__close, .overlay');
+    var modal = $('.modal__div');
+
+    open_modal.click(function (event) {
+        event.preventDefault();
+        var div = $(this).attr('href');
+        overlay.fadeIn(400,
+            function () {
+                $(div)
+                    .css('display', 'flex')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    });
+
+    close.click(function () {
+        modal
+            .animate({
+                    opacity: 0,
+                    top: '45%'
+                }, 200,
+                function () {
+                    $(this).css('display', 'none');
+                    overlay.fadeOut(400);
+                }
+            );
+    });
+});
+//end
+
 
 
 // Инициализация карты
